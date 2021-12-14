baefb45c5fb00a38ab23f823d61a4a78082bacdf
AndrianiClaudio <Claudioandriani1994@gmail.com>
2021-12-14 15:18:03 +0100

    form per stampa risultare non solo su console

--- js/script.js
+++ js/script.js
@@ -17,21 +17,58 @@ function razzo_foreach(array,a,b) {
     });
     return new_array;
 }
-/**
- * 
- * @param {*} array array di elementi
- * @param {*} a numero intero
- * @param {*} b numero intero  maggiore di a
- * @returns array con posizione compresa tra a e b
- */
 function razzo_filter(array,a,b) {
     return array.filter((element,index) => {
         return (index >= a && index <= b);
     });
 }
-
+function showValues(array,a,b) {
+    console.log(razzo_foreach(array, a, b));
+    console.log(razzo_filter(array, a, b));
+}
 const my_array = ['a','b','c','d','e','f',1,2,3,4,5,6,7,8];
-const a = 5;
-const b = 10;
-console.log(razzo_foreach(my_array, a, b));
-console.log(razzo_filter(my_array, a, b));
+let a = document.getElementById('insert_a');
+let b = document.getElementById('insert_b');
+const submit = document.getElementById('submit');
+
+function check (a,b) {
+    const errorInputDiv = document.querySelector('.error-input');
+    const errorInputStrong = '<strong style="color:red;">Inserisci entrambi gli estremi estremi!</strong>';
+    if(a.value != '') {
+        if(b.value != '') {
+            submit.disabled = false;
+            // console.log('if: ',submit.disabled);
+            errorInputDiv.innerHTML = '';
+        } else {
+            submit.disabled = true;
+            // console.log('else1: ',submit.disabled);
+            errorInputDiv.innerHTML = errorInputStrong;
+        }
+    } else {
+        submit.disabled = true;
+        // console.log('else2: ',submit.disabled);
+        errorInputDiv.innerHTML = errorInputStrong;
+    }
+}
+
+b.addEventListener('change',function() {
+    a = document.getElementById('insert_a');
+    b = document.getElementById('insert_b');
+    // (b.value == '') ? submit.disabled = false : firstCondition;
+    check(a,b);
+});
+
+a.addEventListener('change',function () {
+    a = document.getElementById('insert_a');
+    b = document.getElementById('insert_b');
+    // (a.value == '') ? submit.disabled = false : secondCondition ;
+    check(a,b);
+});
+
+submit.addEventListener('click',function(event) {
+    event.preventDefault();
+    a = document.getElementById('insert_a');
+    b = document.getElementById('insert_b');
+    console.log('a: ',a,'b: ',b);
+    showValues(my_array, a.value, b.value);
+});
